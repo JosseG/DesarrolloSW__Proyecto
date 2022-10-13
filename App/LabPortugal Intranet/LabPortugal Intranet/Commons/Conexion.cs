69080cafd37c486f0d8ce9adead1c63ad9be7e1a
Jose <jq994435258@gmail.com>
2022-10-13 11:33:43 -0500

    agregado la conexion y daos

---
+++ App/LabPortugal Intranet/LabPortugal Intranet/Commons/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace LabPortugal_Intranet.Commons
+{
+    public class Conexion
+    {
+
+        private SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+
+        public SqlConnection getConnection()
+        {
+            return connection;
+        }
+
+    }
+}
